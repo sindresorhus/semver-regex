3fe447da70622d59f2e80ce247fecd82fd7521e9
Yanis Benson <yanis.benson@gmail.com>
2019-04-19 15:30:55 +0300

    Improve the regex, require Node.js 8 (#15)

--- test.js
+++ test.js
@@ -10,8 +10,8 @@ const fixture = [
 	'2.7.2+asdf',
 	'1.2.3-a.b.c.10.d.5',
 	'2.7.2-foo+bar',
-	'1.2.3-alpha.10.beta.0',
-	'1.2.3-alpha.10.beta.0+build.unicorn.rainbow',
+	'1.2.3-alpha.10.beta',
+	'1.2.3-alpha.10.beta+build.unicorn.rainbow',
 	'foo 0.0.0 bar 0.0.0'
 ];
 
@@ -31,7 +31,55 @@ test('returns semver on match', t => {
 	t.deepEqual('foo 0.0.0 bar 0.1.1'.match(m()), ['0.0.0', '0.1.1']);
 });
 
-// See #7
-test.failing('does not return tag prefix', t => {
+test('#7, does not return tag prefix', t => {
 	t.deepEqual('v0.0.0'.match(m()), ['0.0.0']);
+});
+
+test('#14, does not match substrings of longer semver-similar strings, respect semver2.0.0 clause 9', t => {
+	const invalidStrings = [
+		'1',
+		'1.2',
+		'1.2.3-0123',
+		'1.2.3-0123.0123',
+		'1.1.2+.123',
+		'+invalid',
+		'-invalid',
+		'-invalid+invalid',
+		'-invalid.01',
+		'alpha',
+		'alpha.beta',
+		'alpha.beta.1',
+		'alpha.1',
+		'alpha+beta',
+		'alpha_beta',
+		'alpha.',
+		'alpha..',
+		'beta',
+		'1.0.0-alpha_beta',
+		'-alpha.',
+		'1.0.0-alpha..',
+		'1.0.0-alpha..1',
+		'1.0.0-alpha...1',
+		'1.0.0-alpha....1',
+		'1.0.0-alpha.....1',
+		'1.0.0-alpha......1',
+		'1.0.0-alpha.......1',
+		'01.1.1',
+		'1.01.1',
+		'1.1.01',
+		'1.2',
+		'1.2.3.DEV',
+		'1.2-SNAPSHOT',
+		'1.2.31.2.3----RC-SNAPSHOT.12.09.1--..12+788',
+		'1.2-RC-SNAPSHOT',
+		'-1.0.3-gamma+b7718',
+		'+justmeta',
+		'9.8.7+meta+meta',
+		'9.8.7-whatever+meta+meta',
+		'99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12'
+	];
+
+	for (const string of invalidStrings) {
+		t.notRegex(string, m());
+	}
 });
