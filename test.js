0ca6460da1a2d75e9a0152bfe4ebc90e83832267
Anton Golub <antongolub@antongolub.com>
2022-05-25 11:14:59 +0300

    refactor: handle some corner cases

--- test.js
+++ test.js
@@ -14,7 +14,6 @@ const fixtures = [
 	'2.7.2-foo+bar',
 	'1.2.3-alpha.10.beta',
 	'1.2.3-alpha.10.beta+build.unicorn.rainbow',
-	// 'foo 0.0.0 bar 0.0.0',
 	'99999.99999.99999'
 ];
 
@@ -23,7 +22,7 @@ test('matches semver versions on test', t => {
 		t.regex(fixture, semverRegex());
 		t.true(semver.valid(fixture) !== null, fixture);
 
-		if (!fixture.startsWith('v')) { // Should we trim v previx?
+		if (!fixture.startsWith('v')) { // Should we trim v prefix?
 			t.deepEqual(fixture.match(semverRegex()), [fixture]);
 		}
 	}
@@ -37,7 +36,8 @@ test('matches semver versions on test', t => {
 test('returns semver on match', t => {
 	t.deepEqual('0.0.0'.match(semverRegex()), ['0.0.0']);
 	t.deepEqual('1.2.3-alpha.10.beta'.match(semverRegex()), ['1.2.3-alpha.10.beta']);
-	t.deepEqual('foo 0.0.0 bar 0.1.1'.match(semverRegex()), ['0.0.0', '0.1.1']);
+	t.deepEqual('1.2.3-alpha.10.beta'.match(semverRegex()), ['1.2.3-alpha.10.beta']);
+	t.deepEqual('0.0.0-foo-bar alpha.beta.1 1.2.3-4'.match(semverRegex()), ['0.0.0-foo-bar', '1.2.3-4']);
 });
 
 test('#7, does not return tag prefix', t => {
@@ -65,7 +65,7 @@ test('#14, does not match sub-strings of longer semver-similar strings, respect 
 		'alpha.',
 		'alpha..',
 		'beta',
-		// '1.0.0-alpha_beta',
+		'1.0.0-alpha_beta',
 		'-alpha.',
 		'1.0.0-alpha..',
 		'1.0.0-alpha..1',
@@ -78,20 +78,19 @@ test('#14, does not match sub-strings of longer semver-similar strings, respect 
 		'1.01.1',
 		'1.1.01',
 		'1.2',
-		// '1.2.3.DEV',
+		'1.2.3.DEV',
 		'1.2-SNAPSHOT',
-		// '1.2.31.2.3----RC-SNAPSHOT.12.09.1--..12+788',
+		'1.2.31.2.3----RC-SNAPSHOT.12.09.1--..12+788',
 		'1.2-RC-SNAPSHOT',
 		'-1.0.3-gamma+b7718',
-		'+justmeta'
-		// '9.8.7+meta+meta',
-		// '9.8.7-whatever+meta+meta',
-		// '99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12'
+		'+justmeta',
+		'9.8.7+meta+meta',
+		'9.8.7-whatever+meta+meta',
+		'99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12'
 	];
 
 	for (const string of invalidStrings) {
 		t.notRegex(string, semverRegex());
-		console.log(string.match(semverRegex()))
 		t.true(semver.valid(string) === null)
 	}
 });
